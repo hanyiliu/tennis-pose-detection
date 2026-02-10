7f6811b10b201dcf1f163cf5f49bc00d116f4b1c
erikagosti <erika.gosti@gmail.com>
2026-02-10 01:39:34 -0800

    feat: created bbox_detection.py
    - bbox detection model

---
+++ models/bbox_detection.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# extracts features with 3 convolution blocks, shrinks the image 3 times with pooling,
+# flattens the result, then uses dense layers to predict 4 bounding box values.
+# nn.Module is PyTorch's base class for all neural network models
+class BBoxDetectionModel(nn.Module):
+    def __init__(self):
+        super(BBoxDetectionModel, self).__init__()
+        
+        # creating convolution layers
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        
+        # a 2x2 window slides over the input feature map
+        # reduces the spatial size of the image by half (75% reduction in total data)
+        # (B, C, H, W) -> (B, C, H/2, W/2)
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        self.fc1 = nn.Linear(64 * 90 * 160, 256)
+        # maps 256 features from fc1 to 4 outputs: [x_min, y_min, width, height]
+        self.fc2 = nn.Linear(256, 4)
+        
+    def forward(self, x):
+        # extracts features, then adds non-linearity, then is shrunk spatial size by 2
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        
+        print(x.shape)
+        # flattening
+        # ex: (B, 64, 90, 160) -> (B, 921600)
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        # shape: (B, 4)
+        x = self.fc2(x)
+        
+        return x
+    
+# loss function: criterion = nn.SmoothL1Loss()
+# lowk what is SmoothL1 loss compared to MSE loss..
+    
+# for images, boxes in dataloader:
+
+#     optimizer.zero_grad()
+
+#     preds = model(images)
+
+#     loss = criterion(preds, boxes)
+
+#     loss.backward()
+#     optimizer.step()
+
+# train on 50 images first to check for overfitting, then scale up
