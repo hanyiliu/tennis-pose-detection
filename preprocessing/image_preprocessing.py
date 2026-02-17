98d0699c20330a841440b9905bb0c5ada35f98c7
Lucas Gobaco <lcgobaco@ucdavis.edu>
2026-02-17 11:04:44 -0800

    transform to transforms

--- preprocessing/image_preprocessing.py
+++ preprocessing/image_preprocessing.py
@@ -1,12 +1,12 @@
-import torchvision.transforms as transform
+import torchvision.transforms as transforms
 
 def convert_image_to_tensor(image):
     """
     Converts a PIL RGB image to a tensor representation.
     Shape: (# of channels, image_height, image_width)
     """
-    transform = transform.Compose([
-        transform.ToTensor(),
+    transforms = transforms.Compose([
+        transforms.ToTensor(),
     ])
 
-    return transform(image)
+    return transforms(image)
