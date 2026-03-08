d7cc9fb222b3da1a6ff05f1837220972ef7f0e4b
Hanyi Liu <hanyiliu0@gmail.com>
2026-03-08 15:56:52 -0700

    feat: tie together inference pipeline

--- inference/inference.py
+++ inference/inference.py
@@ -1,24 +1,39 @@
 from preprocessing.image_preprocessing import convert_image_to_tensor
-from preprocessing.pil_preprocessing import crop_pil, letterbox_resize
+from preprocessing.pil_preprocessing import crop_pil, letterbox_resize, norm_bbox_to_xyxy_pixels
 from preprocessing.tensor_preprocessing import heatmaps_to_keypoints, normalize_keypoints_xy
 
 
 class InferencePipeline:
-    def __init__(self, bbox_detection, keypoint_detection, pose_detection, keypoint_image_size=(256, 256)):
+    def __init__(
+        self,
+        bbox_detection,
+        keypoint_detection,
+        pose_detection,
+        bbox_image_size=(256, 256),
+        keypoint_image_size=(256, 256),
+    ):
         self.bbox_detection = bbox_detection
         self.keypoint_detection = keypoint_detection
         self.pose_detection = pose_detection
+        self.bbox_image_size = bbox_image_size
         self.keypoint_image_size = keypoint_image_size
 
     def run_pipe(self, pil_image):
+        bbox_device = next(self.bbox_detection.parameters()).device
+        keypoint_device = next(self.keypoint_detection.parameters()).device
+        pose_device = next(self.pose_detection.parameters()).device
+
         # image to tensor for bounding box detection (stage 1)
-        tensor = convert_image_to_tensor(pil_image)
-        bbox = self.bbox_detection(tensor.unsqueeze(0)) 
+        bbox_resized_img = letterbox_resize(pil_image, size=self.bbox_image_size)
+        tensor = convert_image_to_tensor(bbox_resized_img).to(bbox_device)
+        bbox = self.bbox_detection(tensor.unsqueeze(0)).squeeze(0)
+        image_width, image_height = pil_image.size
+        bbox_xyxy = norm_bbox_to_xyxy_pixels(bbox, image_width, image_height)
 
         # crop and resize for keypoint detection (stage 2)
-        cropped_img = crop_pil(pil_image, bbox)
+        cropped_img = crop_pil(pil_image, bbox_xyxy)
         resized_img = letterbox_resize(cropped_img, size=self.keypoint_image_size)
-        keypoint_tensor = convert_image_to_tensor(resized_img)
+        keypoint_tensor = convert_image_to_tensor(resized_img).to(keypoint_device)
 
         heatmaps = self.keypoint_detection(keypoint_tensor.unsqueeze(0))
         # convert heatmaps to keypoints
@@ -30,6 +45,6 @@ class InferencePipeline:
         keypoints = normalize_keypoints_xy(keypoints, H, W)
 
         # pose classification, inputting matrix of keypoints of shape (18, 3) (stage 3)
-        pose = self.pose_detection(keypoints)
+        pose = self.pose_detection(keypoints.to(pose_device))
 
         return pose
