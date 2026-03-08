c70480cb0f67454f2dff7d9249293d852594fc0d
Hanyi Liu <hanyiliu0@gmail.com>
2026-03-08 16:17:42 -0700

    Squashed commit of the following:
    commit d7cc9fb222b3da1a6ff05f1837220972ef7f0e4b
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Sun Mar 8 15:56:52 2026 -0700
    
        feat: tie together inference pipeline
    
    commit ec7f6ad476743133ea37902e9d86c3af9ee98647
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Sun Mar 8 15:56:27 2026 -0700
    
        feat: add eval for each stage
    
    commit 681b088757d4a9724d154f0d13f3055f273d2072
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Sun Mar 8 15:02:26 2026 -0700
    
        feat: implement eval for each model
    
    commit 738829e8e684bba8b2a89b4ff79ae8f8c0c52dac
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Sun Mar 8 14:47:19 2026 -0700
    
        Revert "feat: increase bbox model resolution, use letterbox to resize"
    
        This reverts commit f481de3248fd5082bd8c057389d8e5bd8f424732.
    
    commit f481de3248fd5082bd8c057389d8e5bd8f424732
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Fri Mar 6 17:22:51 2026 -0800
    
        feat: increase bbox model resolution, use letterbox to resize
    
    commit 1aad2f93842a5e23ac92df696f45b0e94ba4731e
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Fri Mar 6 17:03:51 2026 -0800
    
        refactor: avoid name conflict
    
    commit a4132d9b2b8b19f23f0b252f2e9751b022c36996
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Mon Mar 2 16:22:17 2026 -0800
    
        feat: outline inference notebook
    
    commit 7e1c860e2de2c8c9e2b4876208118bd98c7cbc02
    Merge: bbb428d 35acf2b
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Mon Mar 2 15:42:55 2026 -0800
    
        Merge branch 'main' into TPD-23-tie-model-architecture-all-together-2
    
    commit bbb428d92cc1b7e8424cb8ec18647d8f16c1486a
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Mon Mar 2 15:38:07 2026 -0800
    
        feat: add visualization notebooks for bbox and pose
    
    commit 3e85f4f28732d99b058252558fa4a6834e73ddce
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Mon Mar 2 15:37:56 2026 -0800
    
        refactor: sort notebooks into subdirs
    
    commit 879a8553a636c88da6b6005a1b941bf369718035
    Author: Hanyi Liu <hanyiliu0@gmail.com>
    Date:   Mon Mar 2 13:34:25 2026 -0800
    
        feat: train bbox model

--- train/bbox_train.py
+++ train/bbox_train.py
@@ -1,14 +1,19 @@
 from torch.utils.data import DataLoader, random_split
 import torch
 import os
+import sys
 
 import argparse
 import torch.nn as nn
 from torchvision import transforms
+import kagglehub
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
 from data.bbox_dataset import TennisBBoxDataset
 from models.bbox_detection import BBoxDetectionModel
-import kagglehub
-import os
 
 if os.path.basename(os.getcwd()) == "eda":
     os.chdir("..")
