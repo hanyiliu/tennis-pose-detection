895b4ec6ad385ff04b8e38de063c0fd4bf37a80c
Hanyi Liu <hanyiliu0@gmail.com>
2026-03-09 21:07:49 -0700

    feat: init back and model final pred component

---
+++ frontend/tpd-demo/src/app/components/back-button/back-button.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-back-button',
+  imports: [],
+  templateUrl: './back-button.html',
+  styleUrl: './back-button.scss',
+})
+export class BackButton {}
---
+++ frontend/tpd-demo/src/app/components/model-final-prediction/model-final-prediction.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-model-final-prediction',
+  imports: [],
+  templateUrl: './model-final-prediction.html',
+  styleUrl: './model-final-prediction.scss',
+})
+export class ModelFinalPrediction {}
